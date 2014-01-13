a7fa52bb04ed06884a395f59efb89f5f5d21ece1
Peters <jordane.peters@epitech.eu>
2014-01-13 21:04:57 +0100

    signup

--- Dev/testJordane/js/index.js
+++ Dev/testJordane/js/index.js
@@ -1,11 +1,43 @@
 $(document).ready(function(){
-	$('#button1').on('click', function() {
-		$('p').css('background-color', 'red');
-		$('input').addClass('error');
+
+	window.onload=onLoad();
+
+
+	function onLoad()
+	{
+		$('#forgotUsernameTitle').hide();
+		$('#forgotPasswordTitle').hide();
+	};
+
+	$('#forgotPassword').on('click', function() {
+
+		$('#normalContent').hide();
+		$('#forgotContent').show();
+		$('#normalTitle').hide();
+		$('#forgotPasswordTitle').show();
 
 	});
-	$('input').change(function() {
-		
-		console.log($(this).val());
+
+	$('#forgotUsername').on('click', function() {
+		$('#normalContent').hide();
+		$('#forgotContent').show();
+		$('#normalTitle').hide();
+		$('#forgotUsernameTitle').show();
+	
 	});
+
+	$('#returnToLogIn').on('click', function() {
+		$('#normalContent').show();
+		$('#forgotContent').hide();
+		$('#normalTitle').show();
+		$('#forgotPasswordTitle').hide();
+		$('#forgotUsernameTitle').hide();
+	});
+
+/*
+	$('#createAnAccount').on('click', function() {
+		$('#');
+	});
+*/
+	
 });
